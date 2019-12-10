a77d49e3f35e16a966718d33b2bc84bdce5257a9
alexgmm <alexandre.guedes.martins@gmail.com>
2019-12-10 17:37:06 -0300

    +

--- Biblis/target/generated-sources/annotations/com/anne/biblis/entity/Emprestimo_.java
+++ Biblis/target/generated-sources/annotations/com/anne/biblis/entity/Emprestimo_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-08T18:02:02")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-08T18:37:46")
 @StaticMetamodel(Emprestimo.class)
 public class Emprestimo_ { 
 
